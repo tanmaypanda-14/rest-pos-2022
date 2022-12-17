783e8c816d1599c917b4c9f4033a5ad699bfd784
Tanmay Panda <87299490+uneconomicalfairy14@users.noreply.github.com>
2022-12-17 04:05:29 +0000

    --09:35

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # rest-pos-2022
+
+on priority:
+    Bill generation page[] --tanmay 
+    Customer page[] -- tanmay 
+    Add a stats page[] -- divyansh
+    Fix: duplicate order entries in cart[] -- 
+    Fix: Added item not showing up in home page[] --  
+    Fix: Unique cart for every unique member[] --  
+-- 21st dec fixes deadline
+
+--till jan 15th-- 
+
+Feature: Add Tables[]
+Feature: Profile Management: admin(can choose), employee(can choose), owner(all access), ;[]
+
+-- till feb 15th --
+
+Feature: Kitchen display system[]
+
+-- till march 1st --
+
+
+
+
+
--- client/src/components/DLayout.js
+++ client/src/components/DLayout.js
@@ -1,8 +1,8 @@
-import { MenuFoldOutlined, MenuUnfoldOutlined, ShoppingCartOutlined, LogoutOutlined, SettingOutlined, UnorderedListOutlined, UserOutlined, HomeOutlined, CopyOutlined } from '@ant-design/icons';
+import { MenuFoldOutlined, MenuUnfoldOutlined, ShoppingCartOutlined, LogoutOutlined, UnorderedListOutlined, UserOutlined, HomeOutlined, CopyOutlined } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import '../styles/Layout.css';
 
@@ -29,29 +29,26 @@ const DLayout = (props) => {
                 <div className="logo">
                     <h3>REST-2022</h3>
                 </div>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={window.location.pathname}>
-                    <Menu.Item key='home' icon=<HomeOutlined /> >
-                        <Link to='/home'>Home</Link>
-                    </Menu.Item>
-                    <Menu.Item key='items' icon=<UnorderedListOutlined /> >
-                        <Link to='/items'>Items</Link>
-                    </Menu.Item>
-                    <Menu.Item key='bills' icon=<CopyOutlined /> >
-                        <Link to='/bills'>Bills</Link>
-                    </Menu.Item>
-                    <Menu.Item key='customers' icon=<UserOutlined /> >
-                        <Link to='/customers'>Customers</Link>
-                    </Menu.Item>
-                    <Menu.Item key='settings' icon=<SettingOutlined /> >
-                        <Link to='/settings'>Settings</Link>
-                    </Menu.Item>
-                    <Menu.Item key='logout' icon={<LogoutOutlined />} onClick={()=>{
-                        localStorage.removeItem('rest-user')
-                        navigate('/login')
-                    }} >
-                        Logout
-                    </Menu.Item>
-                </Menu>
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={window.location.pathname}
+                    onClick={({ key }) => {
+                        if (key === 'logout') {
+                            localStorage.removeItem('rest-user');
+                            navigate('/login');
+                        } else{
+                            navigate(key);
+                        }
+                    }}
+                    items={[
+                        { label: 'Home', key : "/home", icon: <HomeOutlined /> },
+                        { label: 'Items', key : "/items", icon: <UnorderedListOutlined /> },
+                        { label: 'Bills', key : "/bill", icon: <CopyOutlined /> },
+                        { label: 'Customers', key : "/customer", icon: <UserOutlined /> },
+                        { label: 'Logout', key : "logout", icon: <LogoutOutlined /> }
+                    ]}
+                ></Menu>
             </Sider>
             <Layout className="site-layout">
                 <Header
